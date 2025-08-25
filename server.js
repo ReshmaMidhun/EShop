9d9a0fd17eb057c51331c51d594260fbeae2f6d9
ReshmaMidhun <154419791+ReshmaMidhun@users.noreply.github.com>
2025-08-25 15:30:41 +0400

    Update server.js

--- server.js
+++ server.js
@@ -666,10 +666,11 @@ app.get("/getProducts", (req, res) => {
     })
 
 })
-const PORT = 80; 
+const PORT = process.env.PORT || 80; 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
 
 
 
